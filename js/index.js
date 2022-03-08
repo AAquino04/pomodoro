7f28ca3b03f2a867e3562c03b5521800ad63486f
Anderson Aquino <johndoe@example.com>
2022-03-08 11:45:44 -0300

    add break timer

--- js/index.js
+++ js/index.js
@@ -2,23 +2,29 @@
 const pomodoroData = localStorage.getItem('data');
 const data = JSON.parse(pomodoroData);
 
-// Elementos do DOM
+// Elementos do DOM para estilo
+const title = document.querySelector("main .work-info h2");
+const innerTimer = document.querySelector("main .inner-timer");
+
+// Elementos do DOM para lógica do timer
 const startBtn = document.getElementById('start');
 const screenTimerMinutes = document.getElementById('timer-min');
 const screenTimerSeconds = document.getElementById('timer-sec');
-const title = document.querySelector("main .work-info h2");
+
+// Seta timer para valores iniciais vindos da home;
 screenTimerMinutes.innerText = data.workTime;
 screenTimerSeconds.innerText = "00";
 
 // Dados do timer
 let { workTime, breakTime, rounds } = data;
 let seconds = 00;
+let onBreak = false;
 
-// Adiciona o evento  de start ao botão
+// Adiciona o evento de start ao botão
 let startTimer;
 startBtn.addEventListener('click', () => {
     if (!startTimer) {
-        startTimer = setInterval(timer, 1000);
+        startTimer = setInterval(timer, 10);
     } else {
         alert("Cronômetro iniciado, aguarde o fim do ciclo atual.")
     }
@@ -26,6 +32,17 @@ startBtn.addEventListener('click', () => {
 
 // Timer
 function timer() {
+    // Para o cronômetro quando chega ao fim
+    if (workTime === 0 && seconds === 0) {
+        clearInterval(startTimer);
+
+        /**
+         * Caso termine o timer de descanso: Reset para o timer de trabalho
+         * Caso contrário: Reset para o timer de descanso.
+         */
+        onBreak ? reset() : breakTimer();
+    }
+
     // Adiciona zero a esquerda dos segundos se forem menos que 10
     if (seconds < 10) {
         screenTimerMinutes.innerText = workTime;
@@ -33,12 +50,6 @@ function timer() {
     } else {
         screenTimerMinutes.innerText = workTime;
         screenTimerSeconds.innerText = seconds;
-    }
-
-    // Para o cronômetro quando chega ao fim
-    if (workTime === 0 && seconds === 0) {
-        clearInterval(startTimer);
-        reset();
     }
 
     // Subtrai um minuto quando os segundos chegam a 0
@@ -55,13 +66,40 @@ function timer() {
  * possibilitando o início de outro round.
  */
 function reset() {
+    onBreak = false;
+    updateStyle(onBreak);
+
     if (rounds > 0) {
         workTime = data.workTime;
         screenTimerMinutes.innerText = workTime;
         screenTimerSeconds.innerText = "00";
         startTimer = null;
-        --rounds;
     } else {
         title.innerText = "Fim!";
+        innerTimer.style = "border-color: #dc0755";
+        title.style = "color: #dc0755";
+    }
+}
+
+function breakTimer() {
+    onBreak = true;
+    updateStyle(onBreak);
+
+    workTime = data.breakTime;
+    screenTimerMinutes.innerText = workTime;
+    screenTimerSeconds.innerText = "00";
+    startTimer = null;
+    --rounds;
+}
+
+function updateStyle(onBreak) {
+    if (onBreak) {
+        title.innerText = "Descanso"
+        innerTimer.style = "border-color: #F2C94C"
+        title.style = "color: #F2C94C"
+    } else {
+        title.innerText = "Trabalho";
+        innerTimer.style = "border-color: #219653"
+        title.style = "color: #219653";
     }
 }
