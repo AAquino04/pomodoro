3d3ec3071f8a9d2ca7da21610889c648c8c9bc7e
Anderson Aquino <johndoe@example.com>
2022-03-08 12:56:58 -0300

    add pause feature

--- js/utils.js
+++ js/utils.js
@@ -9,7 +9,7 @@ let onBreak = false;
 function timer() {
     // Para o cronômetro quando chega ao fim e chama funções de reset
     if (workTime === 0 && seconds === 0) {
-        clearInterval(startTimer);
+        clearInterval(timerState);
 
         /**
          * Caso termine o timer de descanso: Reset para o timer de trabalho
@@ -37,39 +37,73 @@ function timer() {
 }
 
 /**
- * Reseta o timer e seta startTimer para null,
+ * Reseta o timer e seta timerState para null,
  * possibilitando o início de outro round.
  */
 function reset() {
     onBreak = false;
     updateStyle(onBreak);
+    startBtn.innerHTML = "&#9655;"
 
     if (rounds > 0) {
         workTime = data.workTime;
         screenTimerMinutes.innerText = workTime;
         screenTimerSeconds.innerText = "00";
-        startTimer = null;
+        timerState = null;
     } else {
         title.innerText = "Fim!";
         roundBullets.forEach(el => el.style = "color: #dc0755");
         title.style = "color: #dc0755";
         innerTimer.style = "border-color: #dc0755";
+        timerState = "end";
     }
 }
 
 /**
- * Seta o timer para o tempo de descanso e reseta startTimer,
+ * Seta o timer para o tempo de descanso e reseta timerState,
  * possibilitando o início de outro round de trabalho.
  */
 function breakTimer() {
     onBreak = true;
     updateStyle(onBreak);
+    startBtn.innerHTML = "&#9655;"
 
     workTime = data.breakTime;
     screenTimerMinutes.innerText = workTime;
     screenTimerSeconds.innerText = "00";
-    startTimer = null;
+    timerState = null;
     --rounds;
+}
+
+
+/**
+ * Pausa o timer, muda o símbolo no botão de start,
+ * por fim, retorna o tempo do momento da pausa em um array: 
+ * [minutos, segundos].
+ */
+function pauseTimer() {
+    clearInterval(timerState);
+    timerState = "paused";
+
+    startBtn.innerHTML = "&#9655;"
+
+    const pausedMinutes = parseInt(screenTimerMinutes.innerText);
+    const pausedSeconds = parseInt(screenTimerSeconds.innerText);
+
+    return [pausedMinutes, pausedSeconds]
+}
+
+/** 
+ * Muda os minutos e segundos para os valores recebidos por parâmetro,
+ * altera o ícone do botão de start,
+ * reinicia o cronômetro com os novos valores;
+ */
+function resumeTimer(pausedMinutes, pausedSeconds) {
+    startBtn.innerHTML = "&#8214;"
+
+    workTime = pausedMinutes;
+    seconds = pausedSeconds;
+    timerState = setInterval(timer, 100);
 }
 
 // Estiliza os elementos para informar em que etapa o usuário está.
@@ -79,12 +113,10 @@ function updateStyle(onBreak) {
         roundBullets.forEach(el => el.style = "color: #F2C94C");
         innerTimer.style = "border-color: #F2C94C";
         title.style = "color: #F2C94C";
-        roundBullets.style = "color: #F2C94C";
     } else {
         title.innerText = "Trabalho";
         roundBullets.forEach(el => el.style = "color: #219653");
         innerTimer.style = "border-color: #219653";
         title.style = "color: #219653";
-        roundBullets.style = "color: #219653";
     }
 }
